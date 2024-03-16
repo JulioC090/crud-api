491470461661f80a4b4eb5c2e6429105403b621e
JulioC090 <juliocziviani@gmail.com>
2024-03-16 01:36:12 -0300

    chore: add prettier and eslint

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import Fastify from 'fastify';
 const fastify = Fastify();
 
-fastify.get('/products', function (request, reply) {
-    reply.send(
-        { 
-            products: [
-                {name: "Produto 1", description: "Descrição do Produto", price: 19.99}
-            ] 
-        }
-    )
-})
+fastify.get('/products', (request, reply) => {
+  reply.send({
+    products: [
+      { name: 'Produto 1', description: 'Descrição do Produto', price: 19.99 },
+    ],
+  });
+});
 
-fastify.listen({ port: 3000 }, function (err, address) {
-    if (err) {
-      fastify.log.error(err)
-      process.exit(1)
-    }
-    console.log(`Server is now listening on ${address}`);
-})
+fastify.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Server is now listening on ${address}`);
+});
