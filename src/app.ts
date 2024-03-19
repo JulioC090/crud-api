3b506e4f13f393accd330cf49d600a7d2758483b
JulioC090 <juliocziviani@gmail.com>
2024-03-18 23:08:15 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import productRoutes from '@/routes/productRoutes';
+import cors from '@fastify/cors';
 import fastify from 'fastify';
 
 function buildServer(port: number = 3000, opts = {}) {
   const app = fastify(opts);
+  app.register(cors);
   app.register(productRoutes);
   app.listen({ port }, (err, address) => {
     console.log(`Server is now listening on ${address}`);
