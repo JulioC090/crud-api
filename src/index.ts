8019614e145aaf3f4891efc6d292a9e875ea403e
JulioC090 <juliocziviani@gmail.com>
2024-03-18 22:49:10 -0300

    chore: add docker container configuration with MongoDB

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import buildServer from '@/app';
 import MongoDBHelper from '@/helpers/MongoDBHelper';
 import 'dotenv/config';
 
-const port = parseInt(process.env.PORT as string);
+const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const port = parseInt(process.env.SERVER_PORT as string);
 
-MongoDBHelper.connect(process.env.MONGO_URL as string)
+MongoDBHelper.connect(
+  `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
+)
   .then(async () => {
     buildServer(port);
   })
